197a207531d64d9f3ba2740f6916bea396b81e3d
pablofernandonavarro <pablofernandonavarro@gmail.com>
2026-01-27 11:52:42 -0300

    calenadr admin4

--- resources/views/livewire/settings/appearance.blade.php
+++ resources/views/livewire/settings/appearance.blade.php
@@ -2,10 +2,28 @@
     @include('partials.settings-heading')
 
     <x-settings.layout :heading="__('Appearance')" :subheading=" __('Update the appearance settings for your account')">
-        <flux:radio.group x-data variant="segmented" x-model="$flux.appearance">
+        <flux:radio.group 
+            x-data="{ 
+                currentTheme: $flux.appearance,
+                changeTheme(newTheme) {
+                    $flux.appearance = newTheme;
+                    // Recargar la página después de un breve delay para que el cambio se guarde
+                    setTimeout(() => {
+                        window.location.reload();
+                    }, 100);
+                }
+            }" 
+            variant="segmented" 
+            x-model="currentTheme"
+            @change="changeTheme($event.target.value)"
+        >
             <flux:radio value="light" icon="sun">{{ __('Light') }}</flux:radio>
             <flux:radio value="dark" icon="moon">{{ __('Dark') }}</flux:radio>
             <flux:radio value="system" icon="computer-desktop">{{ __('System') }}</flux:radio>
         </flux:radio.group>
+        
+        <flux:callout color="blue" class="mt-4">
+            <strong>Nota:</strong> Al cambiar el tema, la página se recargará automáticamente para aplicar los cambios correctamente.
+        </flux:callout>
     </x-settings.layout>
 </section>
