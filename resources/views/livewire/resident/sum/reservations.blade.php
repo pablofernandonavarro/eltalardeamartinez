f745dd61308c96ff474904599b3b9a129319c050
pablofernandonavarro <pablofernandonavarro@gmail.com>
2026-01-27 11:34:21 -0300

    calenadr admin4

--- resources/views/livewire/resident/sum/reservations.blade.php
+++ resources/views/livewire/resident/sum/reservations.blade.php
@@ -40,7 +40,7 @@
 
                 @if ($unitId)
                     @if ($isResponsible)
-                        <span class="inline-flex items-center rounded-full bg-green-100 px-3 py-1 text-xs font-medium text-green-800 dark:bg-green-900/50 dark:text-green-200">
+                        <span class="inline-flex items-center rounded-full bg-green-10 px-3 py-1 text-xs font-medium text-green-800 dark:bg-green-900/50 dark:text-green-200">
                             <svg class="mr-1.5 h-3 w-3" fill="currentColor" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
                             </svg>
